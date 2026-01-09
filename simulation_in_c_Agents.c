8b08dcefa04f3678596c7b6e17966ef9f1a68207
arborter <110191602+arborter@users.noreply.github.com>
2026-01-09 06:21:42 -0500

    testing functionality
    testing functionality of struct and functions. next i am planning on creating self generating stocks based on automata inspired by the Game of Life,

--- simulation_in_c_Agents.c
+++ simulation_in_c_Agents.c
@@ -3,6 +3,9 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+char SYMBOL[16];
 
 // struct who represents a stock
 typedef struct {
@@ -14,16 +17,20 @@ typedef struct {
 } Stock;
 
 // a struct who creates a stock
-Stock create_stock(const char *symbol, double price, int volume){
-  Stock s;
-  snprintf(s.symbol, sizeof(s.symbol), "%s", symbol);  // safe copy, always null-terminated
-  s.price = price;
-  s.volume = volume;
-  // Below are additions to the dynamic properties of the stock.
+Stock create_stock(const char *symbol, double price, int volume)
+{
+    Stock s;
+    snprintf(s.symbol, sizeof(s.symbol), "%s", symbol);
+    s.price = price;
+    s.volume = volume;
+    //s.volatility = 0.0;
+      // Below are additions to the dynamic properties of the stock.
   //s.timestamp = timestamp;
   // volatility is market update (changes dictated by new cycle, geopolitics, natural disasters, changes in industry, etc.)
   //s.volatility = volatility; // in this case market volatility is dictated by temperature sensor.
+    return s;
 }
+
 
 // Parameters for Geometric Brownian Motion
 double S = 100.0;       // Initial price
@@ -32,11 +39,19 @@ const double sigma = 0.2;     // Volatility (annualized)
 const double dt = 1.0 / 252;  // Time step (1 trading day in years)
 
 // Generate a normally distributed random number using Box-Muller transform
-double randNormal() {
-  double u1 = (double)random(1, 10000) / 10000.0;
-  double u2 = (double)random(1, 10000) / 10000.0;
-  return sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
+//double randNormal() {
+//  double u1 = (double)random(1, 10000) / 10000.0;
+//  double u2 = (double)random(1, 10000) / 10000.0;
+// return sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
+//}
+
+double randNormal(void)
+{
+    double u1 = ((double)rand() + 1.0) / ((double)RAND_MAX + 2.0);
+    double u2 = ((double)rand() + 1.0) / ((double)RAND_MAX + 2.0);
+    return sqrt(-2.0 * log(u1)) * cos(2.0 * M_PI * u2);
 }
+
 
 // Simulate one price tick
 double nextPrice() {
@@ -50,17 +65,20 @@ double nextPrice() {
 // This function injects a randomn variable to
 //inject into the stock
 double inject_volatility(void){
-	// seeding for a number
-	srand(time(NULL));
 	double volatile_var = (double)rand() / (double)RAND_MAX;
 	return volatile_var;
 }
 
-int main(void) {
-  double price = nextPrice();
-  printf("Price: "); printf(price);
-  Serial.print(" ");
-  delay(150);
-  getTemp();
-  Serial.println();
+int main(void)
+{
+    srand((unsigned int)time(NULL));
+
+    double price = nextPrice();
+    printf("Price: %f\n", price);
+
+    scanf("%15s", SYMBOL);
+    Stock s = create_stock(SYMBOL, 100.00, 100);
+    printf("STOCK: %s \n", s.symbol);
+
+    //return 0;
 }
